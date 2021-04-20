da1f2814f863289f94d9a7eaa0b5053acfaee683
Mairis Karklins <zibitijs@gmail.com>
2021-04-20 21:47:41 +0300

    centos pildit message uzdevumu, add funkcija ir, bet nemaku iznest uz ekrana un nezinu ka talak turpinat, lai tai pievienotu 'update' un 'delete'funckijas

--- message.php
+++ message.php
@@ -21,25 +21,35 @@
 
         <button type="submit">Submit</button>
     </form>
-</div>
 
-<?php
-print_r($_GET);
-if (
-    array_key_exists('email', $_GET) &&
-    array_key_exists('phone', $_GET) &&
-    array_key_exists('description', $_GET) &&
-    is_string($_GET['email']) &&
-    is_string($_GET['phone']) &&
-    is_string($_GET['description'])
-) {
-    $db->add(
-        'messages',
-        [
-            'email' => $_GET['email'],
-            'phone' => $_GET['phone'],
-            'description' => $_GET['description']
-        ]
-    );
-}
-?>
+    <?php
+    if (
+        array_key_exists('email', $_GET) &&
+        array_key_exists('phone', $_GET) &&
+        array_key_exists('description', $_GET) &&
+        is_string($_GET['email']) &&
+        is_string($_GET['phone']) &&
+        is_string($_GET['description'])
+    ) {
+        $db->add(
+            'messages',
+            [
+                'email' => $_GET['email'],
+                'phone' => $_GET['phone'],
+                'description' => $_GET['description']
+            ]
+        );
+    }
+    print_r($_GET); // parāda ko satur masīvs $_GET(url joslas)
+    foreach ($db->getAll() as $row) {
+        echo "<p>";
+        echo "<b>" . $row['id'] . "</b>";
+        echo "username:" . text($row['username']);
+        echo " email:" . text($row['email']);
+        echo " <a href='?delete=" . $row['id'] . "'>delete</a>";
+        echo " <a href='?update=" . $row['id'] . "'>update</a>";
+        echo "</p>";
+    }
+
+    ?>
+</div>
